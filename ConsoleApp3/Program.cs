0297dcae96c8863f446731e7a6d5815865443888
taras-gr <tar.grytsenko@gmail.com>
2025-07-06 14:25:50 +0300

    Module 7: Advanced Parallel Programming: Understanding Locking and Shared Variable

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,118 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static object syncRoot = new object();
+        static object lock1 = new object();
+        static object lock2 = new object();
+
+        static AsyncLocal<decimal?> threadLocalValue = new();
+        static async Task Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            //int total = 0;
+
+            //Parallel.For(0, 100, (i) =>
+            //{
+            //    var tempRes = Compute(i);
+            //    lock (syncRoot)
+            //    {
+            //        total += tempRes;
+            //    }
+            //});
+
+            //Parallel.For(0, 100, (i) =>
+            //{
+            //    var tempRes = Compute(i);
+            //    Interlocked.Add(ref total, (int)tempRes);
+            //});
+
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    total += Compute(i);
+            //}
+
+
+            //var t1 = Task.Run(() =>
+            //{
+            //    lock(lock1)
+            //    {
+            //        Thread.Sleep(1);
+            //        lock (lock2)
+            //        {
+            //            Console.WriteLine("hello");
+            //        }
+            //    }
+            //});
+            //var t2 = Task.Run(() =>
+            //{ 
+            //    lock(lock2)
+            //    {
+            //        Thread.Sleep(1);
+            //        lock (lock1)
+            //        {
+            //            Console.WriteLine("world");
+            //        }
+            //    }
+            //});
+
+            //await Task.WhenAll(t1, t2);
+
+            //var cancellationTokenSource = new CancellationTokenSource();
+            //cancellationTokenSource.CancelAfter(2000); // Cancel after 1 second
+
+            //var parallelOptions = new ParallelOptions
+            //{
+            //    MaxDegreeOfParallelism = 1,
+            //    CancellationToken = cancellationTokenSource.Token
+            //};
+            //int total = 0;
+            //try
+            //{
+            //    Parallel.For(0, 100, parallelOptions, (i) =>
+            //    {
+            //        Interlocked.Add(ref total, (int)Compute(i));
+            //    });
+            //}
+            //catch (OperationCanceledException ex)
+            //{
+            //    Console.WriteLine("Cancellation requested");
+            //    Console.WriteLine(total);
+            //}
+            threadLocalValue.Value = 200;
+            var options = new ParallelOptions
+            {
+                MaxDegreeOfParallelism = 1
+            };
+            Parallel.For(0, 100, options, async (i) =>
+            {
+                var currentValue = threadLocalValue.Value;
+                threadLocalValue.Value = Compute(i);
+            });
+
+
+            //Console.WriteLine(total);
+            //Console.WriteLine($"It took: {stopwatch.ElapsedMilliseconds}ms to run");
+            Console.ReadLine();
+        }
+
+        static Random random = new Random();
+        static decimal Compute(int value)
+        {
+            var randomMilliseconds = random.Next(1, 50);
+            var end = DateTime.Now + TimeSpan.FromMilliseconds(randomMilliseconds);
+
+            while (DateTime.Now < end)
+            {
+                // Simulate some work
+            }
+
+            return value + 0.5m;
+        }
+    }
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static ThreadLocal<int> _threadLocalValue = new ThreadLocal<int>();
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(() =>
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    _threadLocalValue.Value += 1;
+                    Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId}: Value: {_threadLocalValue.Value}");
+                }                
+                
+            });
+            Thread t2 = new Thread(() =>
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    _threadLocalValue.Value += 1;
+                    Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId}: Value: {_threadLocalValue.Value}");
+                }
+            });
+            t1.Start();
+            t2.Start();
+
+            t1.Join();
+            t2.Join();
+            Console.WriteLine(_threadLocalValue.Value);
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
